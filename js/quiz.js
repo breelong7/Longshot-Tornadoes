0a21292da9765595598de3965a5511e2605ad173
breelong7 <breelong7@gmail.com>
2019-09-24 20:07:31 -0700

    added header, main, and footer content to index html. Instantiated drinks in js constructor function

--- js/quiz.js
+++ js/quiz.js
@@ -9,4 +9,15 @@ function Drink(drinkName, isAlcoholic){
 }
 
 new Drink('Orange Juice', false);
+new Drink('Coca-cola', false);
+new Drink('Iced Tea', false);
+new Drink('Lemonade', false);
+new Drink('La croix', false);
+new Drink('Beer', true);
+new Drink('Whiteclaw', true);
+new Drink('Wine', true);
+new Drink('Margarita', true);
+new Drink('Long Island Iced Tea', true);
+
+
 
