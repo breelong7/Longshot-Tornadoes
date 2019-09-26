26ce1c4d19cf9e40e40b5d5484c7e4c6e6a64af3
breelong7 <breelong7@gmail.com>
2019-09-25 18:10:42 -0700

    added ids to HTML file to pull elements into JS. Started on event listener and handle click function

--- js/quiz.js
+++ js/quiz.js
@@ -1,11 +1,11 @@
 
-var allDrinks = [];
+var allDrinksArr = [];
 
 function Drink(drinkName, isAlcoholic){
   this.drinkName = drinkName;
   this.isAlcoholic = isAlcoholic;
 
-  allDrinks.push(this);
+  allDrinksArr.push(this);
 }
 
 new Drink('Orange Juice', false);
@@ -19,5 +19,32 @@ new Drink('Wine', true);
 new Drink('Margarita', true);
 new Drink('Long Island Iced Tea', true);
 
+
+//declare function to generate random drink suggestion
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//get drink choice container element
+var drinkChoiceContainerEl = document.getElementById('drink-choice-container');
+var alcoholicChoiceEl = document.getElementById('alcoholic');
+var nonAlcoholicChoiceEl = document.getElementById('non-alcoholic');
+
+
+drinkChoiceContainerEl.addEventListener('click', handleClick);
+
+
+function handleClick(e) {
+  var drinkChoice = e.target.id;
+  if(drinkChoice === alcoholicChoiceEl){
+    //render alcololic drink from allDrinksArr
+
+  } else if(drinkChoice === nonAlcoholicChoiceEl) {
+  //render non-alcoholic drink choice from allDrinksArr
+  }
+  //append to DOM
+}
+
+handleClick();
 
 
